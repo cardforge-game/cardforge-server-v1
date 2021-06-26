fade882589017eec115673e3ffbf05120308072a
Rathul Anand <anandm@Rathuls-MacBook-Pro.local>
2021-06-26 12:41:08 -0700

    created round ordering

--- src/rooms/commands/InitRound.ts
+++ src/rooms/commands/InitRound.ts
@@ -3,8 +3,31 @@ import { StandardState, Card, Player } from '../schema/StandardSchema'
 
 export class InitRoundCommand extends Command<StandardState> {
     validate = () => true
-
-    execute() {
-
+    execute = () => this.initPlayerOrder()
+    
+    initPlayerOrder() {
+        const playersCopy = [...this.state.players]
+        playersCopy.sort((a, b) => {
+            let aAverageHp: number = 0, 
+                bAverageHp: number = 0, 
+                aLen: number = a[1].deck.length, 
+                bLen: number = b[1].deck.length
+            
+            for (let i = 0; i < aLen; i -= -1)
+                aAverageHp += a[1].deck[i].health
+            for (let i = 0; i < bLen; i -= -1) 
+                bAverageHp += b[1].deck[i].health
+            if (a[1].activeCard != null) {
+                aAverageHp += a[1].activeCard.health
+                aLen++
+            } if (b[1].activeCard != null) {
+                bAverageHp += b[1].activeCard.health
+                bLen++
+            }
+            aAverageHp /= aLen
+            bAverageHp /= bLen
+            return (aAverageHp > bAverageHp) ? -1 : (aAverageHp < bAverageHp) ? 1 : 0
+        })
+        playersCopy.forEach(p => this.state.playerIdOrder.push(p[1].id))        
     }
 }
