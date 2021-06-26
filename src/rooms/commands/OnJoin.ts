ee0c176e1cd79abd6669056b8fd7e9e82876e8b9
Rathul Anand <anandm@Rathuls-MacBook-Pro.local>
2021-06-26 13:21:14 -0700

    fixed hosting bugs

--- src/rooms/commands/OnJoin.ts
+++ src/rooms/commands/OnJoin.ts
@@ -5,30 +5,28 @@ import { StandardState,Player } from "../schema/StandardSchema";
 
 
 interface IPayload {
-  client: Client;
-  name: string;
+	client: Client;
+	name: string;
 }
 
 export class OnJoinCommand extends Command<StandardState, IPayload> {
   validate({client,name}: IPayload){
-    const players = [...this.state.players.values()]
 
-    if(players.some(p=>p.name === name)){
-        client.send("error","That name is already in use!")
-        return false
-    }
-    
-    return true
+	if ([...this.state.players.values()].some(p => p.name === name)) {
+		client.send("error","That name is already in use!")
+		return false
+	}
+	
+	return true
   }
 
   execute({ client,name }: IPayload) {
-    this.state.players.set(client.sessionId, new Player(
-        {
-            name,
-            money: 300,
-            host: (this.state.players.size === 1 || this.state.players.size == 0)
-        }
-    ))
+	this.state.players.set(client.sessionId, new Player({
+			name,
+			money: 300,
+			host: (this.state.players.size === 1 || this.state.players.size == 0)
+		}
+	))
 
 
   }
